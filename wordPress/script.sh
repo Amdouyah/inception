e485e2ea84240d69c2726b38249214a5abc91712
Amine Douyahia <amdouyah@c1r6p2.1337.ma>
2024-06-02 16:41:55 +0100

    add script

--- wordPress/script.sh
+++ wordPress/script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-wp core download --allow-root
 #download the ltest version of WordPress , run as the root user
+wp core download --allow-root
 
 wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 --alow-root
 
 wp core install --url=${WEBSITE} --title=inception --admin_user=${ADMIN} --admin_pasword=${PASS} --admin_email=${EMAIL} --allow-root
 
+#  change the listening method from a Unix socket to a TCP/IP port
 sed -i "s|listen = /run/php/php8.2-fpm.sock|listen = 0.0.0.0:9000|g" /etc/php/8.2/fpm/pool.d/www.conf
 
-exec php-fpm8.2 -F
 # starts PHP-FPM in the foreground, replacing the current shell process with PHP-FPM. This setup ensures that
+exec php-fpm8.2 -F
