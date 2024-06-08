7af7fcbb3d043940ff24c26a168668d687d97104
Amine Douyahia <amdouyah@c1r6p2.1337.ma>
2024-06-08 21:32:03 +0100

    done

--- wordPress/script.sh
+++ wordPress/script.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-#download the ltest version of WordPress , run as the root user
-wp core download --allow-root
+mkdir -p /run/php
 
-wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 --alow-root
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-wp core install --url=${WEBSITE} --title=inception --admin_user=${ADMIN} --admin_pasword=${PASS} --admin_email=${EMAIL} --allow-root
+chmod +x wp-cli.phar 
 
-#  change the listening method from a Unix socket to a TCP/IP port
+mv wp-cli.phar /usr/local/bin/wp
+
 sed -i "s|listen = /run/php/php8.2-fpm.sock|listen = 0.0.0.0:9000|g" /etc/php/8.2/fpm/pool.d/www.conf
 
-# starts PHP-FPM in the foreground, replacing the current shell process with PHP-FPM. This setup ensures that
+mkdir -p /var/www/html/wordpress 
+
+cd /var/www/html/wordpress
+
+wp core download --allow-root
+#download the ltest version of WordPress , run as the root user
+
+wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=mariadb:3306
+
+wp core install --allow-root --url=${WEBSITE} --title=inception --admin_user=${ADMIN} --admin_password=${PASS} --admin_email=${EMAIL}
+
+wp user create --allow-root ${USER}  ${USER_MAIL} --role=author --user_pass=${USER_PASS}
+
 exec php-fpm8.2 -F
+# starts PHP-FPM in the foreground, replacing the current shell process with PHP-FPM. This setup ensures that
